4f17846146e47414e24e2a2482c43242737b7ed7
Isaac Kerson <ikerson@gmail.com>
2022-09-29 15:47:56 +0700

    update asset files

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "cv.pdf"
-profile_pic = current_dir / "assets" / "profile-pic.png"
+profile_pic = current_dir / "assets" / "profile-pic.jpg"
 
 # page settings
 PAGE_TITLE = "Digital CV | Isaac Kerson"
